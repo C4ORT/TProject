c0676b112e8dda70114bbd89b9a998042bfdc8c3
C4ORT <molrom2003@mail.ru>
2024-06-19 22:11:38 +0300

    add search area

--- WebTableEditor/streamlitTable.py
+++ WebTableEditor/streamlitTable.py
@@ -33,6 +33,11 @@ cursor.execute("SELECT * FROM appTP_customer")
 
 rows = cursor.fetchall()
 
+text_search = st.text_input("Search...", value="")
+
+
+
+
 increment=0
 
 for row in rows:
@@ -69,5 +74,27 @@ connection.close()
 #------------------------
 df = pd.DataFrame(rows, columns=["id", "name", "surname","lastname", "gender","status", "office","work_phone", "cellphone", "position","photo", "created", "photo_blob"])
 # Отображение таблицы на Streamlit
-st.dataframe(df, column_config={"photo_blob": st.column_config.ImageColumn()})
+
+m1 = df["id"].astype(str).str.contains(text_search)
+m2 = df["name"].str.contains(text_search)
+m3 = df["surname"].str.contains(text_search)
+m4 = df["lastname"].str.contains(text_search)
+m5 = df["gender"].str.contains(text_search)
+m6 = df["status"].str.contains(text_search)
+m7 = df["office"].str.contains(text_search)
+m8 = df["work_phone"].str.contains(text_search)
+m9 = df["cellphone"].str.contains(text_search)
+m10 = df["position"].str.contains(text_search)
+m11 = df["created"].str.contains(text_search)
+
+
+
+
+
+df_search = df[m1 | m2 | m3 | m4 | m5 | m6 | m7 | m8 | m9 | m10 | m11]
+
+if text_search:
+    st.dataframe(df_search, column_config={"photo_blob": st.column_config.ImageColumn()})
+
+# st.dataframe(df, column_config={"photo_blob": st.column_config.ImageColumn()})
 
